0eca8c65cdb39c3f247088b3260e8a5645f562df
Lokesh-Kudipudi <lokeshkudipudi2006@gmail.com>
2026-06-18 14:37:59 +0530

    feat: add dvc_push Airflow DAG for daily DVC dataset push to DagsHub

---
+++ backend/dags/dvc_push.py
@@ -0,0 +1,32 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+}
+
+with DAG(
+    "dvc_push",
+    default_args=default_args,
+    description="Push latest dataset version to DagsHub remote storage",
+    schedule="0 1 * * *",  # Run daily at 1:00 AM
+    start_date=datetime(2026, 6, 17),
+    catchup=False,
+    tags=["forecast_ai", "dvc"],
+) as dag:
+
+    push_to_dagshub = BashOperator(
+        task_id="dvc_push_to_remote",
+        bash_command="cd /opt/airflow && "
+                     "dvc remote modify myremote --local user \"$DAGSHUB_USERNAME\" && "
+                     "dvc remote modify myremote --local password \"$DAGSHUB_TOKEN\" && "
+                     "dvc push",
+    )
+
+    push_to_dagshub
